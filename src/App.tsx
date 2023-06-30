46a00a21e1c5b9cf6718bf810dba4a811b57ba70
Verde <luigi.verde1@walgreens.com>
2023-06-30 16:33:07 +0200

    passing entire children

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-   let items = [
-      "Bari",
-      "Bitonto",
-      "Bitetto",
-      "Bitritto",
-      "Binetto",
-   ];
-
-   const handleSelectItem = (item: string) => {
-      console.log(item);
-   };
-
    return (
       <div>
-         <ListGroup
-            items={items}
-            heading="Cities"
-            onSelectItem={handleSelectItem}
-         />
+         <Alert>
+            Hello <span>Wolrd</span>
+         </Alert>
       </div>
    );
 }
